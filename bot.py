8fd6810174e14d29f77f278e3687aa332e10ebe9
Student <tytyty.tema@gmail.com>
2022-04-20 14:26:38 +0300

    я лох

--- bot.py
+++ bot.py
@@ -1,10 +1,14 @@
 from telegram.ext import MessageHandler, ConversationHandler, CommandHandler, \
     Filters, Updater, CallbackContext
-from telegram import Update
+from telegram import Update, ReplyKeyboardMarkup
 from key import TOKEN
 
 
 WAIT_NAME, WAIT_SEX, WAIT_GRADE = range(3)
+
+sex_keyboard = [
+    ["М", "Ж"]
+]
 
 
 def main():
@@ -25,7 +29,7 @@ def main():
         fallbacks=[MessageHandler(Filters.text("cancel"), cancel)]
     )
 
-    dispatcher.add_hander(meet_handler)
+    dispatcher.add_handler(meet_handler)
 
     updater.start_polling()
     print("Бот запущен")
@@ -43,6 +47,7 @@ def meet(update: Update, context: CallbackContext):
     update.message.reply_text(
         'привет, ЧМО!'
     )
+    context.user_data["user"] = {}
     return ask_name(update, context)
 
 
@@ -54,11 +59,36 @@ def ask_name(update: Update, context: CallbackContext):
 
 
 def ask_sex(update: Update, context: CallbackContext):
-    pass
+    """
+    TODO add keyboard
+    TODO add validation
+    :param update:
+    :param context:
+    :return:
+    """
+    message = update.message
+    if not validate_name(message.text):
+        message.reply_text(
+            "Неверное имя!"
+        )
+        return WAIT_NAME
+
+    context.user_data["user"]["name"] = message.text
+    message.reply_text(
+        "Выберите ваш пол",
+        reply_markup=ReplyKeyboardMarkup(sex_keyboard, resize_keyboard=True)
+    )
+    return WAIT_SEX
 
 
 def ask_grade(update: Update, context: CallbackContext):
-    pass
+    if not validate_sex(update.message.text):
+        update.message.reply_text(
+            "Ты ЧМО!\n"
+            "Мы Русские у нас два пола!!1"
+        )
+        return WAIT_SEX
+    return WAIT_GRADE
 
 
 def greed(update: Update, context: CallbackContext):
@@ -67,6 +97,14 @@ def greed(update: Update, context: CallbackContext):
 
 def cancel(update: Update, context: CallbackContext):
     return ConversationHandler.END
+
+
+def validate_name(name: str) -> bool:
+    return True
+
+
+def validate_sex(sex: str) -> bool:
+    return sex in sex_keyboard[0]
 
 
 if __name__ == '__main__':
